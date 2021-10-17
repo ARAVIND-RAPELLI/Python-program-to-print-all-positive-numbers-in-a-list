f786d8217dab331272c2910d3b3a8c170d330574
ARAVIND-RAPELLI <82324195+ARAVIND-RAPELLI@users.noreply.github.com>
2021-10-17 17:37:02 +0530

    Add files via upload

---
+++ print only positive numbers.py
@@ -0,0 +1,10 @@
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+for num in list1:
+     if num >= 0:
+       print(num)
+print("are the positive numbers in list 1")
+for num in list2:
+    if num >= 0:
+       print(num)
+print("are the positive numbers in list 2")
